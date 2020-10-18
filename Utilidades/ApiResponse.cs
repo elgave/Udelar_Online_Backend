7875a3cd02e20b725abd93fc9cb8cdf2f4013dd1
fuzzfactory0 <zekrom325@gmail.com>
2020-10-18 19:08:58 -0300

    Agregados paquetes nuget

---
+++ Utilidades/ApiResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Utilidades
+{
+    public class ApiResponse<T>
+    {
+        public T Data { get; set; }
+        public bool Success { get; set; } = true;
+        public int Status { get; set; } = 200;
+        public string Message { get; set; } = null;
+    }
+}
