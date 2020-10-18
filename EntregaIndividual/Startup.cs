de0cf2e404dfbe53cf789f0a5db45c05dc8b6c31
fuzzfactory0 <zekrom325@gmail.com>
2020-10-18 19:10:15 -0300

    Inyeccion de dependencias

--- EntregaIndividual/Startup.cs
+++ EntregaIndividual/Startup.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using BusinessLayer;
+using DataAccessLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,12 +29,25 @@ namespace EntregaIndividual
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ICarreraManager, CarreraManager>();
+            services.AddScoped<IUsuarioManager, UsuarioManager>();
+            services.AddScoped<IFacultadManager, FacultadManager>();
+            services.AddScoped<ICursoManager, CursoManager>();
             services.AddControllers();
+            services.AddAutoMapper(typeof(Startup));
+            services.AddDbContext<MyContext>(opt =>
+               opt.UseSqlServer(Configuration.GetConnectionString("TSIDB")));
+
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("AllowOrigin");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
