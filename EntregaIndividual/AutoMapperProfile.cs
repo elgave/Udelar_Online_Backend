d462b370afcc19a5dd8b307f7a11448b83688ac8
fuzzfactory0 <zekrom325@gmail.com>
2020-10-18 19:10:25 -0300

    Perfil de automapper

---
+++ EntregaIndividual/AutoMapperProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Utilidades.DTOs.Curso;
+using Utilidades.DTOs.Usuario;
+using Utilidades.DTOs.Facultad;
+using Utilidades.DTOs.Carrera;
+
+namespace TSI
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Usuario, GetUsuarioDTO>()
+                .ForMember(dto => dto.Cursos, u => u.MapFrom(u => u.UsuariosCursos.Select(uc => uc.Curso)));
+            CreateMap<AddUsuarioDTO, Usuario>();
+
+            CreateMap<Curso, GetCursoDTO>();
+            CreateMap<AddCursoDTO, Curso>();
+
+            CreateMap<Facultad, GetFacultadDTO>();
+            CreateMap<AddFacultadDTO, Facultad>();
+
+            CreateMap<Carrera, GetCarreraDTO>();
+            CreateMap<AddCarreraDTO, Carrera>();
+        }
+    }
+}
