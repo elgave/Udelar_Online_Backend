3967e2633d4672f5c1af7e482cd849cfb624ffe7
Peter Rodriguez <peter-126@hotmail.com>
2020-11-10 07:38:08 -0300

    no message

---
+++ DataAccessLayer/EncuestaFacultad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    public class EncuestaFacultad
+    {
+        public int IdEncuesta { get; set; }
+
+        public int IdFacultad { get; set; }
+        public string Fecha { get; set; }
+
+        [ForeignKey("IdEncuesta")]
+        public virtual Encuesta Encuesta { get; set; }
+
+        [ForeignKey("IdFacultad")]
+        public virtual Facultad Facultad { get; set; }
+    }
+
+}
